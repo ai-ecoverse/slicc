ba2cf4afaebecee0fa303ea77a3d1cf69c859b94
Lars Trieloff <lars@trieloff.net>
2026-08-03 10:13:06 +0200

    Spike: land libghostty-spm in the iOS build
    Agent-Id: agent-e917e3ad-8687-41ee-a224-68c2916eb0d6
    Linked-Note-Id: 6ca15433-ba52-4ba7-b5b6-61199194a3aa

--- packages/ios-app/Package.swift
+++ packages/ios-app/Package.swift
@@ -13,6 +13,7 @@ let package = Package(
         )
     ],
     dependencies: [
+        .package(url: "https://github.com/Lakr233/libghostty-spm", exact: "1.3.2"),
         .package(url: "https://github.com/stasel/WebRTC.git", .upToNextMajor(from: "150.0.0")),
         .package(path: "../swift-traysession"),
     ],
@@ -20,6 +21,7 @@ let package = Package(
         .target(
             name: "SliccFollower",
             dependencies: [
+                .product(name: "GhosttyTerminal", package: "libghostty-spm"),
                 .product(name: "WebRTC", package: "WebRTC"),
                 .product(name: "SliccTraySession", package: "swift-traysession"),
             ],
